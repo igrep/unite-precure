bd21d34852bc66c1130a0736034dc6bd81f97879
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-12-23 23:20:13 +0900

    set multiline message as lines list.
    vim's append() function appends lines list by line

--- autoload/unite/sources/collect_precures.rb
+++ autoload/unite/sources/collect_precures.rb
@@ -1,5 +1,9 @@
 require 'rubicure'
 
+def to_vim_lines multiline_string
+  "[#{multiline_string.each_line.map {|l| %Q'"#{l.strip}"' }.join(','.freeze)}]"
+end
+
 candidates = Rubicure::Girl.names.map do|name|
   girl = Rubicure::Girl.find name
 
@@ -8,8 +12,8 @@ candidates = Rubicure::Girl.names.map do|name|
   r << %Q'"source__name": "#{girl.precure_name} / #{girl.human_name}",'
   r << %Q'"source__human_name": "#{girl.human_name}",'
   r << %Q'"source__precure_name": "#{girl.precure_name}",'
-  r << %Q'"source__attack": "#{girl.attack_messages[1]}",'
-  r << %Q'"source__transform": "#{girl.transform_message}",'
+  r << %Q'"source__attack": #{to_vim_lines girl.attack_messages[1]},'
+  r << %Q'"source__transform": #{to_vim_lines girl.transform_message},'
   r << '"kind": "precure",'
   r << '}'.freeze
 end
