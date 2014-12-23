e44be9ac2bf119cdd5c5ed5aba1c352ec291a08a
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-12-23 21:17:48 +0900

    list up precure keys

--- autoload/unite/sources/collect_precures.rb
+++ autoload/unite/sources/collect_precures.rb
@@ -1,7 +1,11 @@
 require 'rubicure'
 
+candidates = Rubicure::Girl.names.map do|name|
+  %Q'{ "word": "#{name}", "kind": "common" }'
+end
+
 command_to_set_precures = <<EOS
-let l:precures = [ { "word": "ruby", "kind": "common" } ]
+let l:precures = [#{candidates.join ', '.freeze}]
 EOS
 
 VIM.command(command_to_set_precures)
